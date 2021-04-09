fb4b75ebb333b42cdd552d9ca37e9cd1b448a2df
Wen Lin <linw@vmware.com>
2021-04-10 00:10:19 +0800

    remove wait_until_primary_down

--- src/test/isolation2/sql/fts_session_reset.sql
+++ src/test/isolation2/sql/fts_session_reset.sql
@@ -12,25 +12,11 @@ select pg_reload_conf();
 
 create table test_fts_session_reset(c1 int);
 
-CREATE or REPLACE FUNCTION wait_until_primary_down()
-RETURNS VOID AS
-$$
-begin /* in func */
-  for i in 1..120 loop /* in func */
-    if (select status = 'd' from gp_segment_configuration where content = 0 and role = 'm') then /* in func */
-      return; /* in func */
-    end if; /* in func */
-    perform pg_sleep(1); /* in func */
-  end loop; /* in func */
-end; /* in func */
-$$ language plpgsql;
-
 1:BEGIN;
 -- let the dispatcher create a gang
 1:insert into test_fts_session_reset select * from generate_series(1,20);
 1:select gp_inject_fault('fts_conn_startup_packet', 'error', dbid) from gp_segment_configuration where role='p' and content=0;
 1:select gp_request_fts_probe_scan();
-1:select wait_until_primary_down();
 -- At this point, content 0 mirror is promoted and the primary is marked down. 
 -- the gang used by the previous insert is no longer valid. It must be destroyed 
 -- and the transaction must be aborted.
